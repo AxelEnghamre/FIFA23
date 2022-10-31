07daea7705ef8faaf3e2faba433971866e2d4dd6
MagnusVV <svartkub@msn.com>
2022-10-31 14:06:14 +0100

    Update Roles.sql
    Added foreign keys!

--- Create tables/Roles.sql
+++ Create tables/Roles.sql
@@ -5,5 +5,7 @@ player_id INT,
 match_id INT,
 minutes_played INT,
 role VARCHAR(30),
-PRIMARY KEY (id)
+PRIMARY KEY (id),
+FOREIGN KEY (player_id) REFERENCES Players(id),
+FOREIGN KEY (match_id) REFERENCES Matches(id)
 )
