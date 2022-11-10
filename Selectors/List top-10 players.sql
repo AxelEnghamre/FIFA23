296ad85928d03d60aeb32d32295c5bcc2dad6cfd
Axel Enghamre <axel@enghamre.se>
2022-11-10 16:09:31 +0100

    Create List top-10 players.sql

---
+++ Selectors/List top-10 players.sql
@@ -0,0 +1,31 @@
+--List top 10 playesrs
+SELECT
+Players.team_id,
+Players.first_name,
+Players.last_name,
+
+COUNT(
+CASE
+WHEN Shots.player_shooter_id = Players.id
+THEN TRUE
+END) AS goals,
+
+COUNT(
+CASE
+WHEN Shots.player_assist_id = Players.id
+THEN TRUE
+END) AS assists
+
+FROM Shots
+
+INNER JOIN Players
+ON Players.id = Shots.player_shooter_id
+OR Players.id = Shots.player_assist_id
+
+
+WHERE Shots.is_goal = 1
+
+GROUP BY Shots.player_shooter_id
+
+ORDER BY goals * 100 + assists DESC --One goal is 100 times more worth then one assist
+LIMIT 10
